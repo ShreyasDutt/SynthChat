ffeaaafe390ba6a05f10e3eecd7d8343356b47ec
Shreyas Dutt <151562739+ShreyasDutt@users.noreply.github.com>
2025-08-13 16:21:47 -0700

    User CRUD

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -20,8 +20,35 @@ export async function POST(req: NextRequest) {
       return new Response('User created successfully', { status: 201 })
     }catch (error) {
       console.error('Error processing user.created event:', error)
-      return new Response('Error processing event', { status: 400 })
     }
+    }
+
+    if (evt.type === 'user.deleted') {
+      try{
+        await dbConnect();
+        const deletedUser = await User.findOneAndDelete({clerkId:evt.data.id});
+        
+          console.log("User deleted with Email: "+deletedUser.Email);
+          return new Response('User deleted successfully', { status: 200 })
+      }
+      catch (error) {
+        console.error('Error processing user.deleted event:', error)
+      }
+    }
+
+    if (evt.type === 'user.updated') {
+      try {
+        await dbConnect();
+        const updatedUser = await User.findOneAndUpdate({
+            clerkId: evt.data.id,
+            FirstName: evt.data.first_name || '',
+            Email: evt.data.email_addresses?.[0]?.email_address || ''
+        })
+        console.log("User updated with Email: "+updatedUser.Email);
+        return new Response('User updated successfully', { status: 200 })
+      } catch (error) {
+        console.error('Error processing user.updated event:', error);
+      }
     }
 
     return new Response('Webhook received', { status: 200 })
